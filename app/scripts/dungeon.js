cfa327f3ff11b899dbeeabd3a6f3729d82232775
Daniel Stern <daniel@danielstern.ca>
2015-03-24 19:04:52 -0400

    all is well

--- app/scripts/dungeon.js
+++ app/scripts/dungeon.js
@@ -7,58 +7,77 @@ var dungeon = {
 			}
 		}
 	},
+	stats:'hp.mp.attack.defense.special.resist.speed.evade.accuracy'.split('.'),
 	calculate:{
 		damage:function(attack,defender){
-			return attack.damage - defender.defense / 10;
+			var d = dungeon.calculate.stats(defender);
+			return attack.damage - d.defense / 10;
+		},
+		stats:function(entity){
+			var calculated = {};
+			var level = dungeon.calculate.level(entity)
+			for (key in entity.stats){
+				calculated[key] =  entity.stats[key] * Math.pow(level,1.2);
+			}
+			console.log('calculated?',calculated,entity)
+
+			return calculated;
+		},
+		level:function(entity){
+			return Math.ceil(entity.experience/1000);
 		}
 	},
 	actions:{
 		attack:function(options){
 			var target = options.target;
+			var stats = dungeon.calculate.stats(this);
 			options.target.takeDamage({
-				damage:this.attack,
+				damage:stats.attack,
 				element:options.element
 			});
+		},
+		defend:function(){
+
 		}
 	},
+	/*
+		Add a new action that entities can do.
+	*/
+	action:function(name,action){
+		this.actions[name] = action;
+	},
+
+	/*
+		Creates a blueprint that returns new instances of a creature.
+	*/
 	entity:function(config){
 		config = config || {};
 
-		var hp = config.hp || 100,
-			mp = config. mp || 10,
-			attack = config.attack || 10,
-			defense = config.defense || 10,
-			special = config.special || 10,
-			resist = config.resist || 10,
-			speed = config.speed || 10,
-			evade = config.speed || 10,
-			accuracy = config.accuracy || 10,
-			actions = ['attack','defend'];
+		var stats = dungeon.stats,
+			actions = config.actions || ['attack','defend'];
 
 		var entity = function(ghost){
-			ghost = ghost || {};
-			return {
-				hp:ghost.hp || hp,
-				mp:ghost.mp || mp,
-				attack:ghost.attack || attack,
-				defense:ghost.defense || defense,
-				special:ghost.special || special,
-				resist:ghost.resist || resist,
+			ghost = ghost || {
+				stats:{}
+			};
+			var spawn = {
+				stats:{},
+				experience:1,
+				hp:10,
+				mp:1,
 				actions:ghost.actions|| actions,
 				action:function(name,options){
-					if (this.actions.indexOf(name)===-1){
-						console.error("Action error, action not available",name);
-					};
-
 					var action = dungeon.actions[name];
 					action.bind(this)(options);
-
 				},
 				takeDamage:function(attack){
 					var damage = dungeon.calculate.damage(attack,this);
 					this.hp-=damage;
 				}
 			}
+
+			stats.forEach(function(s){spawn.stats[s] = ghost.stats[s] || 1});
+			return spawn;
 		};
 
 		return entity;
