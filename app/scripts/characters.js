a3ae43260e9449340bb3cd2a769ec32d3e6f66f5
Daniel Stern <daniel@danielstern.ca>
2015-03-25 15:33:16 -0400

    eh

--- app/scripts/characters.js
+++ app/scripts/characters.js
@@ -1,6 +1,6 @@
 var chars = {
-	'goblin':{
-		name:'goblin',
+	'Fireling':{
+		name:'Fireling',
 		max_hp:100,
 		max_mp:10,
 		ap:0,
@@ -15,79 +15,190 @@ var chars = {
 		damage2x:['water'],
 		damage50:['fire'],
 		damage0:[],
-		actions: ['attack','defend'],
+		actions: ['fire_attack_1','defend','ceilidh'],
+	},
+	'Ghoul':{
+		name:'Ghoul',
+		max_hp:90,
+		max_mp:12,
+		ap:0,
+		atb:0,
+		attack:2,
+		defense:1,
+		resist:2,
+		evasion:3,
+		speed:3,
+		properties:['magical','undead'],
+		ai:'01',
+		team:0,
+		damage2x:['light'],
+		damage50:[''],
+		damage0:['dark'],
+		immune:['petrify'],
+		actions: ['defend'],
 	}
 }
 
 var ai = {
 	'00':function(field,turn){
 		var team = this.team;
+		if (Math.random() < 0.2) {
+			return {
+				action:'ceilidh',
+				target:field.filter(function(a){return a.team != team})[0]
+			};
+		}
 		return {
-			action:Math.random() > 0.5 ? 'attack' : 'defend',
+			action:Math.random() > 0.5 ? 'fire_attack_1' : 'defend',
+			target:field.filter(function(a){return a.team != team})[0]
+		};
+	},
+	'01':function(field,turn){
+		// debugger;
+		var team = this.team;
+		if (Math.random() < 0.3) {
+			return {
+				action:'poison_cloud_1',
+				target:field.filter(dungeon.filters.differentTeam(team))
+			};
+		}
+		return {
+			action:Math.random() > 0.5 ? 'special_attack_1' : 'defend',
 			target:field.filter(function(a){return a.team != team})[0]
 		};
 	}
 }
 
-dungeon.action("attack",{
-	damage:'D1',
-	accuracy:'A1',
-	processor:'P1',
-	element:'E2',
-	name:'N1'
-})
+dungeon.action("fire_attack_1",function(target){
 
-.action("defend",{
-	damage:null,
-	accuracy:null,
-	processor:'P2',
-	element:null,
-	name:'defend'
-})
+	var element = 'fire';
+	var accuracy = 0.7;
+	var damage = this.attack + 5;
 
-var d_damage = {
-	'D1':function(entity){return entity.attack + 5}
-}
-var d_accuracy = {
-	'A1':function(entity){return entity.accuracy}
-}
-var d_processor = {
-	'P1':function(options){
-		var entity = options.entity;
-		var target = options.target
-		var element = options.element;
-		var accuracy = options.accuracy(entity);
-		var damage = options.damage(entity);
+	var hit = dungeon.calculate.hit(this,target);
+	
+	damage = dungeon.calculate.elemental(target,damage,element);
+	damage = dungeon.calculate.physicalDamage(target,damage);
 
+	if (hit) {
+		target.takeDamage(damage);
+		if (Math.random() > 0.2) {
+			target.takeStatus('burn');
+		}
+	};
 		
-		damage = dungeon.calculate.elemental(target,damage,element);
-		damage = dungeon.calculate.physicalDamage(target,damage);
 
-		var hit = dungeon.calculate.hit(entity,target);
+	dungeon.meta.event("Fire Attack",{attacker:this,target:target,element:element,hit:hit,damage:damage});
+})
+.action("special_attack_1",function(target){
 
-		if (hit) {
-			target.takeDamage(damage);
-		};
-		
-		dungeon.meta.event(options.name(),{attacker:entity,target:target,element:element,hit:hit,damage:damage});
-	},
-	'P2':function(){
-		this.defending = true;
-		var expiry = 200;
-		var entity = this;
-		this.onstep(function(a){
-			expiry--;
-			if (expiry===0){
-				this.defending = false;
-				dungeon.meta.event("stopDefend",{attacker:entity});
-			}
-		})
+	var damage = this.special + 4,
+	 	hit = dungeon.calculate.hit(this,target);
 
-		dungeon.meta.event("Defend",{attacker:this});
+	damage = dungeon.calculate.specialDamage(target,damage);
+
+	if (hit) {
+		target.takeDamage(damage);	
 	}
-}
 
-var d_name = {
-	'N1':function(){return 'fire attack'}
-}
+	dungeon.meta.event("Special Attack",{attacker:this,target:target,hit:hit,damage:damage});
+})
+.action("poison_cloud_1",function(targets){
+		var entity = this;
+		targets.forEach(function(target){
+			var hit = Math.random() < 0.3;
+			
+			if (hit) {
+				target.takeStatus('poison');
+			}
 
+			dungeon.meta.event("Poison Cloud",{attacker:entity,target:target,hit:hit});
+		})		
+
+	})
+.action("evil_laugh",function(target){
+		var hit = Math.random() < 0.3;
+		if (hit) {
+			target.takeStatus('fear');
+		}
+
+		dungeon.meta.event("Evil Laugh",{attacker:this,target:target,hit:hit});
+	})
+.action("ceilidh",function(target){
+		var hit = Math.random() < 0.3;
+		if (hit) {
+			target.takeStatus('berserk');
+		}
+
+		dungeon.meta.event("ceilidh",{attacker:this,target:target,hit:hit});
+	})
+.action("Heal",function(target){
+		var recovery = 40 + this.special * 3;
+		var undead = target.properties.undead;
+		if (undead) {
+			target.takeDamage(recovery);
+		} else {
+			target.recoverHP(recovery);
+		}
+
+		dungeon.meta.event("ceilidh",{attacker:this,target:target,hit:hit,undead:undead});
+	})
+.action("defend",function(){
+	this.defending = true;
+	var expiry = 200;
+	var entity = this;
+	this.onstep(function(a){
+		expiry--;
+		if (expiry===0){
+			this.defending = false;
+			dungeon.meta.event("stopDefend",{attacker:entity});
+		}
+	})
+
+	dungeon.meta.event("Defend",{attacker:this});
+})
+.action("petrified",function(){
+	console.log("Petrified:",this.name);
+})
+.action("berserk_attack",function(target){
+	if (target instanceof Array) {
+		target = target[0];
+	}
+	var damage = this.attack + 8;
+	var hit = dungeon.calculate.hit(this,target);
+	
+	damage = dungeon.calculate.physicalDamage(target,damage)
+
+	if (hit) {
+		target.takeDamage(damage);
+	};
+
+	dungeon.meta.event("Berserk Attack",{attacker:this,target:target,hit:hit,damage:damage});
+})
+.status( "poison", {
+            beforeAction: function(stats) {
+                var damage = stats.max_hp * 0.05;
+                stats.hp -= damage;
+                dungeon.meta.event("fireDamage", {
+                    target: this,
+                    damage: damage
+                });
+            }
+
+        })
+.status("burn", {
+            beforeAction: function(stats) {
+                var damage = stats.max_hp * 0.1;
+                if (Math.random() > 0.5) stats.hp -= damage;
+                dungeon.meta.event("poisonDamage", {
+                    target: this,
+                    damage: damage
+                });
+            }
+        })
+.status('stone', {
+    replaceAction: 'petrified'
+})
+.status("berserk", {
+    replaceAction: 'berserk_attack'
+});
